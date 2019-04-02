bac54483d69a6927ef9a3484b8dee78a99b57085
M Zulfikar maylendra <mzulfikarmaylendra@M-Zulfikar-Mey-Lendra.local>
2019-04-02 10:30:04 +0700

    build

---
+++ precache-manifest.20ca2d4a27f8fb2749c67eabe770a02b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "fc489f1ab2699952825a",
+    "url": "/static/js/main.a48502b0.chunk.js"
+  },
+  {
+    "revision": "a7b3c1dfb35e3cfc973d",
+    "url": "/static/js/2.0b1075f3.chunk.js"
+  },
+  {
+    "revision": "fc489f1ab2699952825a",
+    "url": "/static/css/main.1e8d635f.chunk.css"
+  },
+  {
+    "revision": "8c7fb448c37976c79bfa86d4168c2be1",
+    "url": "/index.html"
+  }
+];
